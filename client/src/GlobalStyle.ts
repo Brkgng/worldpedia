65f0bcf8784a05c364b82ea7b8e9d124c7095cfd
Berker <berkergng457@gmail.com>
2022-02-07 14:31:35 +0300

    add global style with styled-components

---
+++ client/src/GlobalStyle.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+	:root {
+		--orange: #F98031;
+		--lightblue: rgba(0, 102, 255, 0.15);
+		--fontSmall: 1rem;
+		--fontMed: 1.2rem;
+		--fontBig: 1.5rem;
+		--fontSuperBig: 2.5rem;
+	}
+
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: "Montserrat", sans-serif;
+	}
+`;
