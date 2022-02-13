030e9387877459f186e881cd506c960865a84649
Berker <berkergng457@gmail.com>
2022-02-13 21:23:49 +0300

    add component Spinner

---
+++ client/src/components/Spinner/Spinner.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// Style
+import { SpinnerStyled } from './Spinner.styled';
+
+export interface Props {
+  size?: number;
+  bgColor?: string;
+  spinColor?: string;
+}
+
+const Spinner: React.FC<Props> = ({ size, bgColor, spinColor }) => {
+  return <SpinnerStyled size={size} bgColor={bgColor} spinColor={spinColor} />;
+};
+
+export default Spinner;
