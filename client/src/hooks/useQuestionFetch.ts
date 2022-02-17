e4bf5987e30aacf030e16ca57bbd8668572cf5bd
Berker <berkergng457@gmail.com>
2022-02-17 14:33:23 +0300

    add local storage

--- client/src/hooks/useQuestionFetch.ts
+++ client/src/hooks/useQuestionFetch.ts
@@ -8,8 +8,12 @@ export const useQuestionFetch = () => {
   const [questions, setQuestions] = useState<IQuestion[]>([] as IQuestion[]);
   const [currQuestion, setCurrQuestion] = useState(0);
 
-  const getQuestions = async () => {
-    const questionArr = await fetchQuestions();
+  const getShuffledQuestions = async () => {
+    // Check if local storage has questions
+    // if not fetch questions
+    const questionStorage = localStorage.getItem('questions');
+    const questionArr = questionStorage ? JSON.parse(questionStorage) : await fetchQuestions();
+    if (!questionStorage) localStorage.setItem('questions', JSON.stringify(questionArr));
     // Shuffle questions then get random 10 question
     const randomNum = Math.floor(Math.random() * 51);
     const shuffled = questionArr.sort(() => 0.5 - Math.random()).slice(randomNum, randomNum + 10);
@@ -17,7 +21,7 @@ export const useQuestionFetch = () => {
   };
 
   useEffect(() => {
-    getQuestions();
+    getShuffledQuestions();
   }, []);
 
   return { questions, currQuestion, setCurrQuestion };
